f2f42a63ca0c15dccfb113e8403474edd50c458e
joshwrn <joshnwarren@gmail.com>
2025-07-02 00:46:34 -0700

    chore: move updateTableSize fn to list file

--- internal/app/update-list.go
+++ internal/app/update-list.go
@@ -12,6 +12,27 @@ import (
 	"github.com/zalando/go-keyring"
 )
 
+func (m *model) updateTableSize() {
+	if m.width > 0 && m.height > 0 {
+		keyWidth := 10
+		typeWidth := 10
+		statusWidth := 30
+		createdWidth := 15
+		summaryWidth := max(20, m.width-keyWidth-typeWidth-statusWidth-createdWidth-12)
+
+		columns := []table.Column{
+			{Title: "Key", Width: keyWidth},
+			{Title: "Type", Width: typeWidth},
+			{Title: "Summary", Width: summaryWidth},
+			{Title: "Status", Width: statusWidth},
+			{Title: "Created", Width: createdWidth},
+		}
+		m.table.SetColumns(columns)
+		m.table.SetWidth(m.width - 2)
+		m.table.SetHeight(m.height - 2)
+	}
+}
+
 type ticketsMsg struct {
 	tickets []jira.JiraTicketsMsg
 	err     error
